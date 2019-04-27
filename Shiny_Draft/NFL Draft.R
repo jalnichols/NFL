deb320883bf7794a200eae7a79cb7c6999af3821
Jake Nichols <nichols.jacob@gmail.com>
2019-04-27 15:32:34 -0400

    Add NFL Draft Shiny app

---
+++ Shiny_Draft/NFL Draft.R
@@ -0,0 +1,78 @@
+
+library(tidyverse)
+library(rvest)
+library(shiny)
+
+#
+
+list_years <- vector("list", 25)
+
+for(y in 1:25) {
+  
+  df <- paste0("https://www.pro-football-reference.com/years/", 1993 + y, "/draft.htm") %>%
+    
+    read_html() %>%
+    html_nodes('table') %>%
+    html_table() %>%
+    .[[1]] %>%
+    janitor::clean_names() %>%
+    mutate(draft_year = y + 1993)
+  
+  list_years[[y]] <- df
+
+}
+
+#
+
+drafted_players <- bind_rows(list_years) %>%
+  select(draft_year,
+         round = x,
+         pick = x_2,
+         team = x_3,
+         player = x_4,
+         position = x_5,
+         age = x_6,
+         retired = x_7,
+         AV = approx_val,
+         games = x_9,
+         college = x_13) %>%
+  
+  filter(!age == "Age") %>%
+  
+  mutate(retired = as.numeric(retired),
+         age = as.numeric(age),
+         games = as.numeric(games),
+         AV = as.numeric(AV),
+         round = as.numeric(round),
+         pick = as.numeric(pick)) %>%
+  
+  mutate(AV = ifelse(is.na(AV), 0, AV),
+         games = ifelse(is.na(games), 0, games),
+         career = (retired - draft_year) + 1,
+         career = ifelse(is.na(career), 0, career),
+         AV_season = AV / games * 16,
+         player = str_replace(player, "HOF", ""),
+         AV_regressed = (AV_season * games) / (games + 32)) %>%
+  
+  mutate(AV_season = ifelse(AV_season == "NaN", 0, AV_season),
+         AV_regressed = ifelse(AV_regressed == "NaN", 0, AV_regressed)) %>%
+
+  group_by(round) %>%
+  mutate(round_baseline = sum(AV, na.rm = T) / sum(career, na.rm = T)) %>%
+  ungroup() %>%
+  
+  mutate(relative_round = AV_regressed - round_baseline) %>%
+  
+  mutate(position2 = ifelse(position %in% c("OLB", "ILB"), "LB",
+                            ifelse(position %in% c("NT", "DT"), "DT",
+                                   ifelse(position %in% c("FB", "RB"), "RB",
+                                          ifelse(position %in% c("DB", "S", "CB"), "DB", position)))))
+
+#
+
+drafted_players$position2 <- factor(drafted_players$position2, levels = c("QB", "RB", "WR", "TE", "C", "G", "T",
+                                                                         "DE", "DT", "LB", "DB", "K", "P", "LS"))
+
+#
+
+write_csv(drafted_players, "Shiny_Draft/data/drafted_players.csv")
---
+++ Shiny_Draft/app.R
@@ -0,0 +1,101 @@
+
+library(shiny)
+library(tidyverse)
+library(extrafont)
+
+# load data
+
+drafted_players <- read_csv("data/drafted_players.csv")
+
+#
+#
+#
+#
+# build Shiny app
+
+ui <- fluidPage(
+  
+  titlePanel("NFL Draft (1994-2018)"),
+  
+  sidebarLayout(
+    
+    position = "right",
+    
+    sidebarPanel(
+      
+      helpText("Visualize NFL Draft results from last 25 years"),
+      
+      selectInput("TEAM",
+                  
+                  label = "Choose team to display",
+                  choices = unique(drafted_players$team),
+                  selected = "PIT")
+      
+    ),
+    
+    mainPanel(
+      
+      plotOutput("nfl_draft")
+      
+    )
+    
+  )
+  
+)
+
+#
+
+server <- function(input, output) {
+  
+  output$nfl_draft <- renderPlot(
+    
+    ggplot(data = drafted_players %>%
+             filter(team == input$TEAM), 
+           aes(x = draft_year, y = round, size = relative_round, color = AV_regressed, label = player))+
+      
+      geom_point(size = 3, color = "black")+
+      ggrepel::geom_label_repel()+
+      
+      labs(x = "",
+           y = "Round Selected",
+           title = paste0("Draft picks for ", input$TEAM))+
+      scale_color_gradient2(low = "dark red", mid = "gray", high = "#37b36c", midpoint = 4)+
+      scale_x_continuous(breaks = seq(1994, 2018, 1))+
+      scale_y_reverse(breaks = seq(1,7,1))+
+      theme(axis.text = element_text(size = 24, face = "bold"),
+            axis.title = element_text(size = 24, face = "bold"),
+            plot.title = element_text(size = 36, face = "bold"),
+            text = element_text(family = "Roboto Condensed")),
+    
+    width = 1200,
+    height = 600)
+  
+  output$nfl_draft <- renderPlot(
+    
+    ggplot(data = drafted_players %>%
+             filter(team == input$TEAM), 
+           aes(x = reorder(paste0(player, "/", draft_year), -AV_regressed), y = relative_round, fill = relative_round))+
+      
+      geom_bar(position = "stack", stat = "identity")+
+      
+      labs(x = "",
+           y = "Round Selected",
+           title = paste0("Draft picks for ", input$TEAM))+
+      scale_fill_gradient2(low = "dark red", mid = "gray", high = "#37b36c", midpoint = 4)+
+      theme(axis.text = element_text(size = 24, face = "bold"),
+            axis.title = element_text(size = 24, face = "bold"),
+            plot.title = element_text(size = 36, face = "bold"),
+            text = element_text(family = "Roboto Condensed")),
+    
+    width = 900,
+    height = 900
+    
+  )
+  
+  
+  
+}
+
+#
+
+shinyApp(ui, server)
